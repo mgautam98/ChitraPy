d9d1aca80bcc995d22cbb9160797b7c0a6b68868
Gautam Mishra <mishragautam96@gmail.com>
2020-05-12 05:42:54 +0530

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ChitraPy",
-    version="0.0.1",
+    version="0.0.2",
     author="Gautam Mishra",
     author_email="mishragautam96@gmail.com",
     description="Digital Image Processing Library",
