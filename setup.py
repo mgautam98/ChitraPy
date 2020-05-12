2af6852336409d8988e72919ba6ee3c88271141f
Gautam Mishra <mishragautam96@gmail.com>
2020-05-12 06:51:49 +0530

    setup changes

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name="ChitraPy",
-    version="0.0.2",
-    author="Gautam Mishra",
-    author_email="mishragautam96@gmail.com",
-    description="Digital Image Processing Library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/mgautam98/ImgPy",
-    packages=find_packages(exclude=['tests']),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-)
+setup()
